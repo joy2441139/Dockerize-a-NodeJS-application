db62c394ea88ae3f3cbf2a000503507f7193a059
joy2441139 <tanvirahmedjoy2441139@gmail.com>
2022-09-26 16:46:03 +0600

    Created func in public\js\script.js to show both current time & current date on index page

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ * @param {Date} date
+ */
+function formatTime(date) {
+    const hour12 = date.getHours() % 12 || 12;
+    const min = date.getMinutes();
+
+    return `${hour12.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}`;
+}
+
+/**
+ * 
+ * @param {Date} date 
+ */
+function formatDate(date) {
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
+
+const now = new Date();
+document.querySelector(".time").textContent = formatTime(now);
+document.querySelector(".date").textContent = formatDate(now);
